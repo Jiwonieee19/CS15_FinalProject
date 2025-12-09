96bbabf2b7980b22dce616dee46397083d88f2cc
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2025-12-09 17:10:52 +0800

    trial

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-import tkinter as tk
+from tkinter import *
+
+# windows = frame/panel
+# widgets = components(button,label,image)
+
+window = Tk()
+window.geometry("540x820")
+window.title("Python - KIOSK")
+window.config(background="#CDAE00")
+
+iconLogo = PhotoImage(file='reso/LOGO.png')
+
+window.iconphoto(True, iconLogo)
+window.mainloop()
